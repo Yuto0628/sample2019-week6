2c4574aa58782be9f20a73de1cf8dffc80f9aaff
Yuto Oshiro <e195732@ie.u-ryukyu.ac.jp>
2019-11-19 10:08:47 +0900

    enshuu

---
+++ src/Main/Readme/md.java
@@ -0,0 +1,8 @@
+package Readme;
+
+public class md {
+    public static void main(String args[]){
+        String str = "読め読め読め読め読め読め読め読め読め読め読め読め読め読め読め読め";
+        System.out.println(str);
+    }
+}
